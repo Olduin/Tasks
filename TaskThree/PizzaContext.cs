72731bdb24be27494512cbe0bca8b473467ab5a3
Евгений Коцюк <olduin@yandex.ru>
2025-07-21 23:23:11 +0400

    Задача 8, функционал реализован

--- TaskThree/PizzaContext.cs
+++ TaskThree/PizzaContext.cs
@@ -20,8 +20,8 @@ public partial class PizzaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost\\MSSQLSERVER01;Database=Pizza;Trusted_Connection=True;TrustServerCertificate=True");
-
+    //=> optionsBuilder.UseSqlServer("Data Source=localhost\\MSSQLSERVER01;Database=Pizza;Trusted_Connection=True;TrustServerCertificate=True");
+       => optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Database=TestKotsiuck;Trusted_Connection=True;TrustServerCertificate=True");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<PizzaModel>(entity =>
